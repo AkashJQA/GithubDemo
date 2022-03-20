a1b3df90ee85743b8bf0f6276ab9192d092a3663
AkashJQA <akash12507@gmail.com>
2022-03-20 16:18:09 +0530

    first comit ha

---
+++ src/test/java/openbrowser/BrowserLounch.java
@@ -0,0 +1,13 @@
+package openbrowser;
+
+
+
+public class BrowserLounch {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("Hi this my first git ");
+		
+	}
+		
+}
